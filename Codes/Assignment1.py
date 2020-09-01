4bf201f17bc6fe0fbf6c0d936126223b1c85610b
rubeenaafreen20 <70516674+rubeenaafreen20@users.noreply.github.com>
2020-09-02 04:00:44 +0530

    Update Assignment1.py

--- Codes/Assignment1.py
+++ Codes/Assignment1.py
@@ -8,36 +8,38 @@ Created on Wed Sep  2 02:50:49 2020
 
 import numpy as np
 import matplotlib.pyplot as plt
-from coeffs import * #Taken from Dr. GVV Sharma's github
+from coeffs import * #Taken from Dr. G.V.V's github
 
 #Line points
 P = np.array([1,2]) 
 Q = np.array([5,3]) 
-M = np.array([2.6,0])  #thepoint on x-axis where reflection takes place
+A = np.array([2.6,0])  #thepoint on x-axis where reflection takes place
 
-slope_PM=(M[1]-P[1])/(M[0]-P[0]) #check the slope of incident ray
-slope_QM=(M[1]-Q[1])/(M[0]-Q[0]) #check the slope of reflected ray
+slope_PA=(A[1]-P[1])/(A[0]-P[0]) #check the slope of incident ray
+slope_QA=(A[1]-Q[1])/(A[0]-Q[0]) #check the slope of reflected ray
 
-print(slope_PM)
+print(slope_PA)
 print("\n")
-print(slope_QM)
+print(slope_QA)
 
 #Draw figure for reflection
 
-x_PM=line_gen(P,M)
-plt.plot(x_PM[0,:],x_PM[1,:],label="PM")
+x_PA=line_gen(P,A)
+plt.plot(x_PA[0,:],x_PA[1,:],label="PA")
 
-x_QM=line_gen(Q,M)
-plt.plot(x_QM[0,:],x_QM[1,:],label="QM")
+x_QA=line_gen(Q,A)
+plt.plot(x_QA[0,:],x_QA[1,:],label="QA")
 
 #plotting and labelling of points
 plt.plot(P[0], P[1], '.')
 plt.text(P[0] * (1 + 0.1), P[1] * (1 + 0.1) , 'P(1,2)')
 plt.plot(Q[0], Q[1], '.')
 plt.text(Q[0] * (1 -0.06), Q[1] * (1+0.05) , 'Q(5,3)')
-plt.plot(M[0], M[1], '.')
-plt.text(M[0] * (1 + 0.1), M[1] * (1 - 0.1) , 'M(13/5,0)')
+plt.plot(A[0], A[1], '.')
+plt.text(A[0] * (1 + 0.1), A[1] * (1 - 0.1) , 'A(13/5,0)')
+
 
 plt.grid()
 plt.xlim(0, 6)
 plt.ylim(-1, 6)
+
